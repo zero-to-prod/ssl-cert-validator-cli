ed4f1781b994d34c3f7164bc0899d81e0a38de0c
david_smith <david_smith@sweetwater.com>
2025-01-27 00:49:22 -0500

    feat/cli Renamed `tests/Unit/ExampleTest.php` to `tests/Unit/SrcCommandTest.php`.

--- tests/Unit/SrcCommandTest.php
+++ tests/Unit/SrcCommandTest.php
@@ -6,21 +6,18 @@ use PHPUnit\Framework\Attributes\Test;
 use Symfony\Component\Console\Application;
 use Symfony\Component\Console\Tester\CommandTester;
 use Tests\TestCase;
-use Zerotoprod\SslCertValidatorCli\ExampleCommand;
+use Zerotoprod\SslCertValidatorCli\SrcCommand;
 
-class ExampleTest extends TestCase
+class SrcCommandTest extends TestCase
 {
     #[Test] public function command(): void
     {
         $application = new Application();
-        $application->add(new ExampleCommand());
+        $application->add(new SrcCommand());
 
-        $command = $application->find('example');
+        $command = $application->find('ssl-cert-validator-cli:src');
         $commandTester = new CommandTester($command);
-        $commandTester->execute([
-            'arg1' => '',
-            '--option1' => '',
-        ]);
+        $commandTester->execute([]);
 
         $commandTester->assertCommandIsSuccessful();
     }

929cebe6040420699d3bfa3eebe1ab65d3bd7495
david_smith <david_smith@sweetwater.com>
2025-01-27 00:39:56 -0500

    feat/cli Updated `src/GetCertCommand.php`.

--- src/GetCertCommand.php
+++ src/GetCertCommand.php
@@ -7,25 +7,18 @@ use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
-use Throwable;
 use Zerotoprod\SslCertValidator\SslCertificate;
 
 #[AsCommand(
-    name: 'ssl-cert-validator-cli:self-signed-cert',
-    description: 'Determines if an SSL certificate is valid for a given hostname.'
+    name: 'ssl-cert-validator-cli:get-cert',
+    description: 'Get SSL certificate for a given hostname.'
 )]
-class SelfSignedCertCommand extends Command
+class GetCertCommand extends Command
 {
-
-    /**
-     * @throws Throwable
-     */
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
         $output->writeln(
-            SslCertificate::isSelfSigned($input->getArgument('hostname'))
-                ? 'true'
-                : 'false'
+            json_encode(SslCertificate::from($input->getArgument('hostname')), JSON_PRETTY_PRINT)
         );
 
         return Command::SUCCESS;

7e52b2b5574769e7a2d02c03aec46fbeb60cb070
david_smith <david_smith@sweetwater.com>
2025-01-29 16:11:42 -0500

    feat/refactor-to-data-models Created `tests/Unit/SelfSignedCertCommandTest.php`.

---
+++ tests/Unit/SelfSignedCertCommandTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Tests\Unit;
+
+use PHPUnit\Framework\Attributes\Test;
+use Symfony\Component\Console\Application;
+use Symfony\Component\Console\Tester\CommandTester;
+use Tests\TestCase;
+use Zerotoprod\SslCertValidatorCli\selfSignedCert\selfSignedCertArguments;
+use Zerotoprod\SslCertValidatorCli\selfSignedCert\selfSignedCertCommand;
+
+class SelfSignedCertCommandTest extends TestCase
+{
+    #[Test] public function command(): void
+    {
+        $Application = new Application();
+        $Application->add(new selfSignedCertCommand());
+
+        $Command = $Application->find(selfSignedCertCommand::signature);
+        $CommandTester = new CommandTester($Command);
+        $CommandTester->execute([
+            selfSignedCertArguments::hostname => selfSignedCertArguments::hostname
+        ]);
+
+        $CommandTester->assertCommandIsSuccessful();
+    }
+}
